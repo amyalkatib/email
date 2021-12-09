91409ee6bc35c4e7622735faaa0abe9eaae3fd50
amyalkatib <91368588+amyalkatib@users.noreply.github.com>
2021-12-09 16:19:10 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,8 +4,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     endmsg = "\r\n.\r\n"
     
 # Choose a mail server (e.g. Google mail server) and call it mailserver
-    mailserver = 'nyu.edu'
-    serverport = 1025
+   # mailserver = 'nyu.edu'
+   # serverport = 1025
 # Create socket called clientSocket and establish a TCP connection with mailserver
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((mailserver, serverport))
