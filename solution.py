8fcca828f47ef7bcc1db0c310c912402a3a22493
amyalkatib <91368588+amyalkatib@users.noreply.github.com>
2021-12-09 02:38:04 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -37,6 +37,29 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     #if recv1[:3] != '250':
        # print ('250 reply not received from server')
 # Send DATA command and print server response.
+    data = "DATA\r\n"
+    clientSocket.send(data.encode())
+    recv4 = clientSocket.recv(1024)
+    #print("After DATA command: "+recv4)
+    #if recv1[:3] != '250':
+        #print('250 reply not received from server.')
+
+# Send message data.
+    subject = "Subject: SMTP mail client testing \r\n\r\n" 
+    clientSocket.send(subject.encode())
+    message = raw_input("Enter your message: \r\n")
+    clientSocket.send(message.encode())
+    clientSocket.send(endmsg.encode())
+    recv_msg = clientSocket.recv(1024)
+    #print("Response after sending message body:"+recv_msg.decode())
+    #if recv1[:3] != '250':
+    #print('250 reply not received from server.')
+
+# Send QUIT command and get server response.
+clientSocket.send("QUIT\r\n".encode())
+message=clientSocket.recv(1024)
+#print (message)
+clientSocket.close()
 
 if __name__ == '__main__':
     smtp_client(1025, '127.0.0.1')
