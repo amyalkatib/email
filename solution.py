35c98985649810440be5506d67ec4d7db7ef8bf9
amyalkatib <91368588+amyalkatib@users.noreply.github.com>
2021-12-11 20:31:51 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -77,6 +77,7 @@ def smtp_client(port=1025, mailserver= '127.0.0.1'):
     QUIT = "QUIT\r\n"
     clientSocket.send(QUIT.encode())
     recv5 = clientSocket.recv(1024).decode()
+    clinetSocket.close()
     # Fill in end
 
 
